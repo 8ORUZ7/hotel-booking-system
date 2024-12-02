ed58bcd2f47204bab51dd36cb9ce6dfae38cd282
#f0f3e3 <170834894+osiristape@users.noreply.github.com>
2024-12-03 03:39:07 +0800

    testing git cmd

---
+++ hotelbookingsystem/target/classes/fxml/FXMLDocumentController.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package fxml;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author shift
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private Button signin_close;
+
+    @FXML
+    private Hyperlink signin_createAccount;
+
+    @FXML
+    private TextField signin_email;
+
+    @FXML
+    private AnchorPane signin_form;
+
+    @FXML
+    private Button signin_loginBtn;
+
+    @FXML
+    private Button signin_minimize;
+
+    @FXML
+    private PasswordField signin_password;
+
+    @FXML
+    private Hyperlink signup_alreadyhaveaccount;
+
+    @FXML
+    private Button signup_btn;
+
+    @FXML
+    private Button signup_close;
+
+    @FXML
+    private TextField signup_email;
+
+    @FXML
+    private AnchorPane signup_form;
+
+    @FXML
+    private Button signup_minimize;
+
+    @FXML
+    private PasswordField signup_password;
+
+    @FXML
+    private TextField signup_username;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        // TODO
+    }    
+    
+}
